501f878403c7237eec2555d4beac179f2a0d343d
Andi Gu <andi.gu.ca@gmail.com>
2019-02-09 16:20:03 -0800

    Added async

--- src/Redux.js
+++ src/Redux.js
@@ -1,15 +1,25 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
 
-export default createStore((state={counter: 0}, action) => {
+const reducer = (state={counter: 0}, action) => {
     switch(action.type) {
         case 'increment': return {counter: state.counter + action.delta}
         default: return state;
     }
-});
+}
+export default createStore(reducer, applyMiddleware(thunk));
 
 export function INCREMENT(x) {
     return {
         type: 'increment',
         delta: x
     }
+}
+
+export function INCREMENT_ASYNC(x) {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(INCREMENT(x));
+        }, 1000);
+    };
 }
--- src/Temp.js
+++ src/Temp.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {INCREMENT} from "./Redux";
+import {INCREMENT, INCREMENT_ASYNC} from "./Redux";
 
 class Temp extends React.Component {
     render() {
         return <div><p>{this.props.counter}</p>
-            <button onClick={() => this.props.dispatch(INCREMENT(5))}>up</button>
+            <button onClick={() => this.props.dispatch(INCREMENT_ASYNC(5))}>up</button>
         </div>
     }
 }
